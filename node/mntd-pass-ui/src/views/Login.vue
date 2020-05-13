132409b11fa44e6f17aaea973a99c3de94654891
kenriortega <kenriortega@gmail.com>
2020-05-12 20:11:13 -0400

    refactoring of Login Component add styles

--- node/mntd-pass-ui/src/views/Login.vue
+++ node/mntd-pass-ui/src/views/Login.vue
@@ -1,48 +1,41 @@
 <template>
-  <div class="bg__login flex flex-col items-center h-screen">
-    <div class=" w-full max-w-xs my-40">
-      <form class="bg-gray shadow-md rounded px-8 pt-6 pb-8 mb-4">
-        <div class="mb-4">
-          <label class="block text-white text-sm font-bold mb-2" for="username"
-            >Usuario</label
-          >
-          <input
-            class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="username"
-            type="text"
-            placeholder="Username"
-            v-model="payload.username"
-          />
-        </div>
-        <div class="mb-6">
-          <label class="block text-white text-sm font-bold mb-2" for="password"
-            >Password</label
-          >
-          <input
-            class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-            id="password"
-            type="password"
-            placeholder="******************"
-            v-model="payload.password"
-          />
-        </div>
-        <div class="flex items-center justify-between">
-          <button
-            class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            @click.prevent="authenticate()"
-          >
-            Ingresar
-          </button>
-        </div>
-      </form>
-      <p class="text-center text-gray-500 text-xs">
-        &copy;2020 mntd-pass-ui.
-      </p>
-    </div>
+  <div class="bg-gray-900-spotify flex flex-col items-center h-screen">
+    <form class="w-full max-w-sm my-40 items-center">
+      <div class="flex items-center border-b border-b-2 border-green-800 py-2">
+        <i class="mr-2 fa fa-user text-gray-700-spotify"></i>
+        <input
+          class="appearance-none bg-transparent border-none w-full text-gray-700-spotify mr-3 py-1 px-2 leading-tight focus:outline-none"
+          type="text"
+          placeholder="User..."
+          v-model="payload.username"
+        />
+      </div>
+      <div class="flex items-center border-b border-b-2 border-green-800 py-2">
+        <i class="mr-2 fa fa-lock text-gray-700-spotify"></i>
+
+        <input
+          class="appearance-none bg-transparent border-none w-full text-gray-700-spotify mr-3 py-1 px-2 leading-tight focus:outline-none"
+          type="password"
+          placeholder="*******"
+          v-model="payload.password"
+        />
+      </div>
+      <div class="flex flex-col items-center py-2">
+        <button
+          class="flex-shrink-0 border-green-900 hover:bg-green-700 text-gray-700-spotify text-sm border-2 hover:text-white py-1 px-2 rounded"
+          type="button"
+          @click.prevent="authenticate()"
+        >
+          Sign In
+        </button>
+      </div>
+    </form>
   </div>
 </template>
 
 <script>
+  import '@/assets/css/all.min.css'
+
   import utils from '@/assets/utils/'
   export default {
     data() {
