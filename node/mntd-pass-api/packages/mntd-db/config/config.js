605385cad640f7fedcdabdba93cddbac57ceea8a
kenriortega <kenriortega@gmail.com>
2020-05-16 01:33:32 -0400

    changed the static settings for env vars into .env

--- node/mntd-pass-api/packages/mntd-db/config/config.js
+++ node/mntd-pass-api/packages/mntd-db/config/config.js
@@ -2,12 +2,13 @@
 
 const path = require('path')
 
+// ok: MIgrate to .env add into you .env file this var and replace for your settings
 module.exports = {
   development: {
-    username: 'postgres',
-    password: 'root',
-    database: 'secretsdb',
-    host: '127.0.0.1',
+    username: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    database: process.env.PG_DATABASE,
+    host: process.env.PG_HOST,
     dialect: 'postgres',
     loggin: false
   },
