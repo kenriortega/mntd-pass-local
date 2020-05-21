b76182ebbc8c1cba9548e474292b539430d9b4c3
kenriortega <kenriortega@gmail.com>
2020-05-21 18:31:40 -0400

    Create Views and content component for a user profile

---
+++ node/mntd-pass-ui/src/views/Profile.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="flex flex-col h-screen text-sm text-gray-400">
+    <div class="flex-1 flex overflow-y-hidden">
+      <!-- Sidebar  -->
+      <sidebar-component />
+      <!-- end Sidebar -->
+      <!-- Content -->
+      <ProfileComponent />
+      <!-- end Content -->
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import SidebarComponent from '@/components/SidebarComponent'
+import ProfileComponent from '@/views/users/'
+export default {
+  name: 'Profile',
+  components: {
+    SidebarComponent,
+    ProfileComponent
+  }
+}
+</script>
---
+++ node/mntd-pass-ui/src/views/users/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="bg-gray-800-spotify flex-1  flex flex-col">
+    <top-bar />
+    <div class="content-spotify overflow-y-auto ">
+      <div class="mx-auto container">
+        <alert-component v-show="errorMSG.error" :errorMSG="errorMSG" />
+        <h2 class="mt-18 text-5xl font-semibold text-white">Profile</h2>
+        <div class="mt-10">
+          <h3 class="font-semiblod text-sm border-b border-gray-900 pb-2">
+            <strong class="text-xl text-green-300">
+              {{ user.fullName || user.username }}</strong
+            >
+            this is your profile...
+          </h3>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { UtilesService } from '@/services/'
+
+import AlertComponent from '@/components/AlertComponent'
+import TopBar from '@/components/TopBarComponent'
+export default {
+  name: 'profile',
+  components: {
+    TopBar,
+    AlertComponent
+  },
+  data: () => ({
+    errorMSG: {},
+    user: {}
+  }),
+  mounted() {
+    this.getUserFromLocalStorage()
+  },
+  methods: {
+    getUserFromLocalStorage() {
+      this.user = UtilesService.getItemStorage('user')
+    }
+  }
+}
+</script>
+
+<style></style>
