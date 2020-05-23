2405d0dbb2451d558b6c0a26da6e4509326faa6c
kenriortega <kenriortega@gmail.com>
2020-05-23 09:25:53 -0400

    change api routes

--- node/mntd-pass-ui/src/constant/apiRoutes.js
+++ node/mntd-pass-ui/src/constant/apiRoutes.js
@@ -1,9 +1,9 @@
 'use strict'
 
-const API_BASE_ROUTE = 'http://localhost:8080'
-
 export const API_ROUTES = {
-  AUTH: `${API_BASE_ROUTE}/auth`,
-  SECRETS: `${API_BASE_ROUTE}/secrets`,
-  GET_SECRET: `${API_BASE_ROUTE}/secrets`
+  AUTH: `/auth`,
+  SECRETS: `/secrets`,
+  GET_SECRET: `/secrets`,
+  STATUS_OK: 200,
+  STATUS_NOT_CONTENT: 204
 }
