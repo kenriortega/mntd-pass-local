dd526cb0fab8bcbff4c42661419401f4e64b2a81
kenriortega <kenriortega@gmail.com>
2020-05-25 21:10:51 -0400

    clean code

--- node/mntd-pass-ui/src/services/AuthService.js
+++ node/mntd-pass-ui/src/services/AuthService.js
@@ -1,12 +1,14 @@
 import axios from '@/http'
 
 import { API_ROUTES } from '@/constant/apiRoutes.js'
+
 class AuthService {
   async auth(username, password) {
     let res = await axios.post(API_ROUTES.AUTH, {
       username,
       password
     })
+
     return res
   }
 }
