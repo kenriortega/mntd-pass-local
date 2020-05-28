f6742653d088fdeb9716401b9de87dda6274429b
kenriortega <kenriortega@gmail.com>
2020-05-27 20:19:38 -0400

    register vee-validate global components

--- node/mntd-pass-ui/src/main.js
+++ node/mntd-pass-ui/src/main.js
@@ -5,7 +5,19 @@ import store from './store'
 import '@/assets/main.scss'
 import '@/assets/css/all.min.css'
 import VTooltip from 'v-tooltip'
+import {
+  ValidationProvider,
+  ValidationObserver,
+  extend,
+  localize
+} from 'vee-validate'
+import * as rules from 'vee-validate/dist/rules'
+import es from 'vee-validate/dist/locale/es.json'
+localize(es)
 
+Object.keys(rules).forEach(rule => extend(rule, rules[rule]))
+Vue.component('ValidationObserver', ValidationObserver)
+Vue.component('ValidationProvider', ValidationProvider)
 Vue.use(VTooltip)
 Vue.config.productionTip = false
 export const bus = new Vue()
