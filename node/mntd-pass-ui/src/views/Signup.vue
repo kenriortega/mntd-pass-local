18a02919d5b5ae644c81183a67aa2aff452bb13d
kenriortega <kenriortega@gmail.com>
2020-05-27 20:25:44 -0400

    add validation simple to form SignUP

--- node/mntd-pass-ui/src/views/Signup.vue
+++ node/mntd-pass-ui/src/views/Signup.vue
@@ -1,53 +1,87 @@
 <template>
   <div class="bg-gray-900-spotify flex flex-col items-center h-screen">
-    <form class="w-full max-w-sm my-40 items-center">
-      <div class="flex flex-col items-center py-2">
-        <alert-component v-show="notificator.error" :errorMSG="notificator" />
-      </div>
-      <div class="flex items-center border-b border-b-2 border-green-800 py-2">
-        <i class="mr-2 fa fa-user text-gray-500"></i>
-        <input
-          class="appearance-none bg-transparent border-none w-full text-gray-500 mr-3 py-1 px-2 leading-tight focus:outline-none"
-          type="text"
-          placeholder="User..."
-          v-model="payload.username"
-        />
-      </div>
-      <div class="flex items-center border-b border-b-2 border-green-800 py-2">
-        <i class="mr-2 fa fa-lock text-gray-500"></i>
-
-        <input
-          class="appearance-none bg-transparent border-none w-full text-gray-500  mr-3 py-1 px-2 leading-tight focus:outline-none"
-          type="password"
-          placeholder="*******"
-          v-model="payload.password"
-        />
-      </div>
-      <div class="flex items-center border-b border-b-2 border-green-800 py-2">
-        <i class="mr-2 fa fa-check text-gray-500"></i>
-
-        <input
-          class="appearance-none bg-transparent border-none w-full text-gray-500  mr-3 py-1 px-2 leading-tight focus:outline-none"
-          type="password"
-          placeholder="*******"
-          v-model="payload.confirmPassword"
-        />
-      </div>
-      <div class="flex flex-col items-center py-2">
-        <button
-          class="text-green-500 hover:text-white hover:bg-green-500 border border-green-500 text-xs font-semibold rounded-full mt-3 px-4 py-1 leading-normal"
-          type="button"
-          @click.prevent="signUp()"
+    <ValidationObserver v-slot="{ invalid }">
+      <form
+        @submit.prevent="signUp()"
+        class="w-full max-w-sm my-40 items-center"
+      >
+        <div class="flex flex-col items-center py-2">
+          <alert-component v-show="notificator.error" :errorMSG="notificator" />
+        </div>
+        <div
+          class="flex items-center border-b border-b-2 border-green-800 py-2"
         >
-          Sign Up
-        </button>
-        <router-link
-          to="/"
-          class="text-gray-500 mr-3 py-1 px-2 leading-tight focus:outline-none"
-          >{{ question }}</router-link
+          <i class="mr-2 fa fa-user text-gray-500"></i>
+          <ValidationProvider
+            name="Username"
+            rules="required|alpha"
+            v-slot="{ errors }"
+          >
+            <span class="text-red-400">{{ errors[0] }}</span>
+            <input
+              class="appearance-none bg-transparent border-none w-full text-gray-500 mr-3 py-1 px-2 leading-tight focus:outline-none"
+              type="text"
+              placeholder="User..."
+              v-model="payload.username"
+            />
+          </ValidationProvider>
+        </div>
+        <div
+          class="flex items-center border-b border-b-2 border-green-800 py-2"
         >
-      </div>
-    </form>
+          <i class="mr-2 fa fa-lock text-gray-500"></i>
+          <ValidationProvider
+            name="password"
+            rules="required|alpha"
+            v-slot="{ errors }"
+          >
+            <span class="text-red-400">{{ errors[0] }}</span>
+            <input
+              class="appearance-none bg-transparent border-none w-full text-gray-500  mr-3 py-1 px-2 leading-tight focus:outline-none"
+              type="password"
+              placeholder="*******"
+              v-model="payload.password"
+            />
+          </ValidationProvider>
+        </div>
+        <div
+          class="flex items-center border-b border-b-2 border-green-800 py-2"
+        >
+          <i class="mr-2 fa fa-check text-gray-500"></i>
+          <ValidationProvider
+            name="passwordConfirm"
+            rules="required|alpha"
+            v-slot="{ errors }"
+          >
+            <span class="text-red-400">{{ errors[0] }}</span>
+            <input
+              class="appearance-none bg-transparent border-none w-full text-gray-500  mr-3 py-1 px-2 leading-tight focus:outline-none"
+              type="password"
+              placeholder="*******"
+              v-model="payload.confirmPassword"
+            />
+          </ValidationProvider>
+        </div>
+        <div class="flex flex-col items-center py-2">
+          <button
+            :disabled="invalid"
+            :class="
+              `text-green-500 hover:text-white hover:bg-green-500 border border-green-500 text-xs font-semibold rounded-full mt-3 px-4 py-1 leading-normal ${
+                invalid ? 'cursor-not-allowed opacity-50' : ''
+              }`
+            "
+            type="submit"
+          >
+            Sign Up
+          </button>
+          <router-link
+            to="/"
+            class="text-gray-500 mr-3 py-1 px-2 leading-tight focus:outline-none"
+            >{{ question }}</router-link
+          >
+        </div>
+      </form>
+    </ValidationObserver>
   </div>
 </template>
 
