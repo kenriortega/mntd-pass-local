19f5bec98f5d99c6de3c4b3e43e2756eb6cc20bc
kenriortega <kenriortega@gmail.com>
2020-05-31 09:51:48 -0400

    refactor: main.js for better organization

--- node/mntd-pass-ui/src/main.js
+++ node/mntd-pass-ui/src/main.js
@@ -4,8 +4,11 @@ import router from './router'
 import store from './store'
 import '@/assets/main.scss'
 import '@/assets/css/all.min.css'
-import VTooltip from 'v-tooltip'
 
+// externals
+import VTooltip from 'v-tooltip'
+import Toaster from 'v-toaster'
+import 'v-toaster/dist/v-toaster.css'
 import {
   ValidationProvider,
   ValidationObserver,
@@ -19,8 +22,12 @@ localize(es)
 Object.keys(rules).forEach(rule => extend(rule, rules[rule]))
 Vue.component('ValidationObserver', ValidationObserver)
 Vue.component('ValidationProvider', ValidationProvider)
+// optional set default imeout, the default is 10000 (10 seconds).
+Vue.use(Toaster, { timeout: 5000 })
 Vue.use(VTooltip)
 Vue.config.productionTip = false
+
+// apps
 export const bus = new Vue()
 new Vue({
   router,
