ba601f1928f7fa722618778e710e69f7e9c7e41b
kenriortega <kenriortega@gmail.com>
2020-06-01 12:50:52 -0400

    refactor: move mock data to constants file

---
+++ node/mntd-pass-ui/src/constant/mock.data.js
@@ -0,0 +1,42 @@
+const MOCK_SECRETS_SERVICE = {
+  MOCK_USER: 'kalix',
+  TOKEN:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7InVzZXJuYW1lIjoia2FsaXgiLCJmdWxsTmFtZSI6ImthbGl4IE9ydGVnYSIsInJvbGUiOiJ3b3JrZXIiLCJjcmVhdGVkQXQiOiIyMDIwLTA1LTE2VDIxOjU5OjA3LjU2OVoifSwiaWF0IjoxNTkxMDI2MTEyLCJleHAiOjE1OTEwMjkxMTJ9.Qb2yXGMCkeTid4PX-8FWDt4MeZHXhf9yaFdtU76jves',
+
+  DATA_IN: {
+    count: 5,
+    data: [
+      {
+        username: 'kalix',
+        name: 'my-secret-38',
+        category: 'payments',
+        createdAt: '2020-05-30T18:58:39.840Z'
+      },
+      {
+        username: 'kalix',
+        name: 'my-secrets',
+        category: 'personal',
+        createdAt: '2020-05-30T18:38:17.780Z'
+      }
+    ]
+  },
+  DATA_OUT: {
+    count: 5,
+    data: [
+      {
+        username: 'kalix',
+        name: 'my-secret-38',
+        category: 'payments',
+        createdAt: '2 days ago'
+      },
+      {
+        username: 'kalix',
+        name: 'my-secrets',
+        category: 'personal',
+        createdAt: '2 days ago'
+      }
+    ]
+  }
+}
+
+export default MOCK_SECRETS_SERVICE
--- node/mntd-pass-ui/src/services/__tests__/SecretsService.spec.js
+++ node/mntd-pass-ui/src/services/__tests__/SecretsService.spec.js
@@ -1,8 +1,6 @@
 import SecretsServcice from '../SecretsService'
 import axios from '@/http'
-let token =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7InVzZXJuYW1lIjoia2FsaXgiLCJmdWxsTmFtZSI6ImthbGl4IE9ydGVnYSIsInJvbGUiOiJ3b3JrZXIiLCJjcmVhdGVkQXQiOiIyMDIwLTA1LTE2VDIxOjU5OjA3LjU2OVoifSwiaWF0IjoxNTkxMDI2MTEyLCJleHAiOjE1OTEwMjkxMTJ9.Qb2yXGMCkeTid4PX-8FWDt4MeZHXhf9yaFdtU76jves'
-
+import MOCK_SECRETS_SERVICE from '@/constant/mock.data.js'
 jest.mock('@/http', () => {
   return {
     // create: jest.fn(),
@@ -12,43 +10,14 @@ jest.mock('@/http', () => {
 describe('Services -> SecretsServcice', () => {
   it(`should get secrets by username`, async () => {
     axios.get.mockResolvedValue({
-      data: {
-        count: 5,
-        data: [
-          {
-            username: 'kalix',
-            name: 'my-secret-38',
-            category: 'payments',
-            createdAt: '2020-05-30T18:58:39.840Z'
-          },
-          {
-            username: 'kalix',
-            name: 'my-secrets',
-            category: 'personal',
-            createdAt: '2020-05-30T18:38:17.780Z'
-          }
-        ]
-      }
+      data: MOCK_SECRETS_SERVICE.DATA_IN
     })
-    let result = await SecretsServcice.getSecrets('kalix', token)
+    let result = await SecretsServcice.getSecrets(
+      MOCK_SECRETS_SERVICE.MOCK_USER,
+      MOCK_SECRETS_SERVICE.TOKEN
+    )
     expect(result).toEqual({
-      data: {
-        count: 5,
-        data: [
-          {
-            username: 'kalix',
-            name: 'my-secret-38',
-            category: 'payments',
-            createdAt: '2 days ago'
-          },
-          {
-            username: 'kalix',
-            name: 'my-secrets',
-            category: 'personal',
-            createdAt: '2 days ago'
-          }
-        ]
-      }
+      data: MOCK_SECRETS_SERVICE.DATA_OUT
     })
   })
 })
